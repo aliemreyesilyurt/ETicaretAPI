d4ab88b63e473ac0f2cc60aaa28a82c3fd17ab74
aliemreyesilyurt <aliemreyesilyurt@hotmail.com>
2024-06-26 16:42:25 +0300

    product, customer. order siniflari olusturuldu connectionString yapisi kuruldu

---
+++ Infrastructure/ETicaretAPI.Persistence/DesignTimeDbContextFactory.cs
@@ -0,0 +1,18 @@
+﻿using ETicaretAPI.Persistence.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace ETicaretAPI.Persistence
+{
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<ETicaretAPIDbContext>
+    {
+        public ETicaretAPIDbContext CreateDbContext(string[] args)
+        {
+            //CMD uzerinden migration alma islemi yapildiginda bu sinif kullanilir
+
+            DbContextOptionsBuilder<ETicaretAPIDbContext> dbContextOptionsBuilder = new();
+            dbContextOptionsBuilder.UseNpgsql(Configuration.ConfigurationString);
+            return new(dbContextOptionsBuilder.Options);
+        }
+    }
+}
