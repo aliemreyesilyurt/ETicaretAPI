18b87b37a9319dd631af4f9a9c606a73aeeea6b0
aliemreyesilyurt <aliemreyesilyurt@hotmail.com>
2024-06-26 14:42:11 +0300

    onion architecture yapisina gore temel olusturuldu

---
+++ Infrastructure/ETicaretAPI.Persistence/Concretes/ProductService.cs
@@ -0,0 +1,34 @@
+﻿using ETicaretAPI.Application.Abstractions;
+using ETicaretAPI.Domain.Entities;
+
+namespace ETicaretAPI.Persistence.Concretes
+{
+    public class ProductService : IProductService
+    {
+        public List<Product> GetProducts()
+            => new()
+            {
+                new()
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Product 1",
+                    Price = 100,
+                    Stock = 10
+                },
+                new()
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Product 2",
+                    Price = 120,
+                    Stock = 20
+                },
+                new()
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Product 3",
+                    Price = 300,
+                    Stock = 5
+                }
+            };
+    }
+}
---
+++ Presentation/ETicaretAPI.API/Controllers/ProductsController.cs
@@ -0,0 +1,24 @@
+﻿using ETicaretAPI.Application.Abstractions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ETicaretAPI.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductService _manager;
+
+        public ProductsController(IProductService manager)
+        {
+            _manager = manager;
+        }
+
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            var product = _manager.GetProducts();
+            return Ok(product);
+        }
+    }
+}
