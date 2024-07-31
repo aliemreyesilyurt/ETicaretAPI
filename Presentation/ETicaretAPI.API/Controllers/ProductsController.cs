6936034ba47712e126f3bf8f3b0a0a29254f61bc
Ali Emre Yeşilyurt <99074640+aliemreyesilyurt@users.noreply.github.com>
2024-07-31 17:09:43 +0300

    Generic Repository design pattern uygulandi, temel yapi olusturuldu

--- Presentation/ETicaretAPI.API/Controllers/ProductsController.cs
+++ Presentation/ETicaretAPI.API/Controllers/ProductsController.cs
@@ -1,4 +1,5 @@
-﻿using ETicaretAPI.Application.Abstractions;
+﻿using ETicaretAPI.Application.Repositories;
+using ETicaretAPI.Domain.Entities;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ETicaretAPI.API.Controllers
@@ -7,17 +8,53 @@ namespace ETicaretAPI.API.Controllers
     [ApiController]
     public class ProductsController : ControllerBase
     {
-        private readonly IProductService _manager;
+        private readonly IProductWriteRepository _productWriteRepository;
+        private readonly IProductReadRepository _productReadRepository;
 
-        public ProductsController(IProductService manager)
+        public ProductsController(IProductWriteRepository productWriteRepository, IProductReadRepository productReadRepository)
         {
-            _manager = manager;
+            _productWriteRepository = productWriteRepository;
+            _productReadRepository = productReadRepository;
         }
 
         [HttpGet]
-        public IActionResult GetProducts()
+        public async Task Get()
         {
-            var product = _manager.GetProducts();
+            await _productWriteRepository.AddRangeAsync(new()
+            {
+                new()
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Product 1",
+                    Price = 20,
+                    CreatedDate = DateTime.UtcNow,
+                    Stock = 10
+                },
+                new()
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Product 2",
+                    Price = 10,
+                    CreatedDate = DateTime.UtcNow,
+                    Stock = 5
+                },
+                new()
+                {
+                    Id = Guid.NewGuid(),
+                    Name = "Product 3",
+                    Price = 50,
+                    CreatedDate = DateTime.UtcNow,
+                    Stock = 15
+                }
+            });
+
+            await _productWriteRepository.SaveAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(string id)
+        {
+            Product product = await _productReadRepository.GetByIdAsync(id);
             return Ok(product);
         }
     }
